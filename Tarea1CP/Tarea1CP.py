503b18db846ddca07ad045dfbb056c8126651130
Lahkrome <mariolp13@gmail.com>
2014-08-21 00:06:34 -0400

    Update Tarea1CP.py

--- Tarea1CP/Tarea1CP.py
+++ Tarea1CP/Tarea1CP.py
@@ -1,26 +1,28 @@
 import time
- 
-def largest_prime_factor(n):
- 
-    largest_factor = 1
- 
-    # remove any factors of 2 first
-    while n % 2 == 0:
-        largest_factor = 2
+
+def factor_primo(n):
+    factor = 1
+    # Eliminar cualquier factor de 2
+    while n % 2 ==0:
+        factor = 2
         n = n/2
- 
-    # now look at odd factors
+
+    # Eliminar el resto de los factores impares
     p = 3
     while n != 1:
         while n % p == 0:
-            largest_factor = p
+            factor = p
             n = n/p
-        p += 2
- 
-    return largest_factor
- 
+        p+=2
+
+    return factor
+
 start = time.time()
-for i in range(100000): a = largest_prime_factor(600851475143)
-elapsed = (time.time() - start)
- 
-print "result %s returned after %s seconds (100,000 iterations)." % (a, elapsed)
+for i in range (100) : a = factor_primo (600851475143)
+
+    #Calculo de tiempo de ejecucion
+
+tiempo = (time.time() - start)
+print "El resultado %s ha retornado despues de %s segundos luego de 100 iteraciones. " % (a, tiempo)    
+
+
