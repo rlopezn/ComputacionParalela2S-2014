ce0bc115597cfa5c247210686e6afe0e5bdc90ce
ccordero <carloscorde@hotmail.com>
2014-08-22 00:49:09 -0400

    revision 2

--- Tarea1CP/Tarea1CP.py
+++ Tarea1CP/Tarea1CP.py
@@ -5,35 +5,23 @@ def contador(resultado):
 		return resultado
 	else :
 		return contador(int(resultado/10))+ resultado%10
-# verifica que el valor ingresado sea menor que 100 (el enunciado de la tarea lo pide asi)
-def menor100(num):
-	if num>100:
-		while (num>100):
-			num = int (input ("Numeros menores que 100 \n "))
-		return num
-	else :
-		return num
+
 #Ingresa  valor de a
-numero= int(input("Ingresar a \n " ))
-numero = menor100(numero)
+numero= 0
 #ingresa el valor de b
-numero2 = int(input("Ingresar b \n"))
-numero2 = menor100(numero2)
-#calcula la potencia
-potencia1 = numero**numero2
-potencia2 = numero2**numero 
-# recibe la suma de la funcion contador
-suma1= contador(potencia1)
-suma2= contador(potencia2)
-#decide cual es mayor
-if(suma1<suma2):
-	print("Suma digital maxima pertenece  a b^a \n ")
-	print(suma2)
-elif(suma1== suma2):
-	print("Suma digital es la misma para ambos casos y es\n ")
-	print(suma2)
-else:
-	print("suma digital maxima pertenece a b^a \n")
-	print(suma1)
-	
+numero2 = 0
+aux = 0
+#itera los valores de a y b
+while (numero<100):
+	numero2=0
+	while (numero2<100):
+		potencia = numero**numero2 
+		if (aux<contador(potencia)):
+			aux = contador(potencia)
+		numero2 =numero2 + 1
+		
+	numero = numero + 1
+print("la suma digital maxima para a^b es \n")
+print(aux)
+
 time.sleep(5.5) 
