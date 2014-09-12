315f5132512981fd0fa944ea9409f1c893f6651b
FcoHernan <FranciscoHRF@gmail.com>
2014-09-11 23:32:31 -0300

    limites para que cada procesador trabaje en la matriz

--- Tarea3CP/Tarea3-Paralela.py
+++ Tarea3CP/Tarea3-Paralela.py
@@ -16,8 +16,9 @@ rank =  comm.rank     # id procesador actual
 size =  comm.size     # cantidad de procesadores a usar
 
 def distribuirEnP(size):
-
     if (rank==0):
+        print ""
+        print ""
         cuoc= (20*20)/size #c : cuociente
         rest= (20*20)%size #r : resto
         ##print "resto:" +str(rest)
@@ -31,6 +32,30 @@ def distribuirEnP(size):
                 conta=conta+cuoc+rest
                 comm.send (conta, dest = p)
                 
+
+def buscarRangos():
+    if rank==0:
+        p=0
+        conta=0
+        rangos=[]
+        valor=0
+        for i in range(20):
+            for j in range (20):
+                if(valor==0):
+                    valor=comm.recv(source=p)
+#                print "i,j = "+str(i)+","+str(j)
+#                print "recv: "+ str(valor)
+#                print "p = "+str(p)
+#                print "conta: "+str(conta)
+                if conta==valor:
+                    rangos = rangos + [i,j]
+                    comm.send(rangos,dest=p)
+                    rangos=[]
+                    p = p + 1
+                    conta = conta + 1
+                    valor=0
+                else:
+                    conta = conta + 1
                 
             
 
@@ -42,88 +67,89 @@ def distribuirEnP(size):
 # que esta compuesta consecutivamente por lso indices y valor del primer valor
 # hasta el 4to valor, que contribuyen a los cuatros valores que la productoria
 # será la mayor, en este caso solo horizontal.
-def buscarHorizontal():
-    i=0
-    j=0
-    c=0
-    multi=1
-    valor_mayor=-1
-    lista= []
-    lista_mayor=[]
-    for i in range (20):
-        for j in range (17):
-            for c in range (4):
-                lista=lista+ [i,j+c,data[i][j+c]]
-                multi=multi*data[i][j+c]
-            result=multi
-            multi=1
-            if (result>valor_mayor):
-                valor_mayor=result
-                lista_mayor=lista
-            else:
-                lista=[]
-    return (valor_mayor,lista_mayor)   
+#def buscarHorizontal():
+#    i=0
+#    j=0
+#    c=0
+#    multi=1
+#    valor_mayor=-1
+#    lista= []
+#    lista_mayor=[]
+#    for i in range (20):
+#        for j in range (17):
+#            for c in range (4):
+#                lista=lista+ [i,j+c,data[i][j+c]]
+#                multi=multi*data[i][j+c]
+#            result=multi
+#            multi=1
+#            if (result>valor_mayor):
+#                valor_mayor=result
+#                lista_mayor=lista
+#            else:
+#                lista=[]
+#    return (valor_mayor,lista_mayor)   
 
 # Al igual que la funcion anterior, la funcion buscarVertical, hará la búsqueda
 # de forma vertical hacia abajo, para no repetir operaciónes y hasta i=17         
             
-def buscarVertical():
-    i=0
-    j=0
-    c=0
-    multi=1
-    valor_mayor=-1
-    lista= []
-    lista_mayor=[]
-    for i in range (17):
-        for j in range (20):
-            for c in range (4):
-                lista=lista+ [i+c,j,data[i+c][j]]
-                multi=multi*data[i+c][j]
-            result=multi
-            multi=1
-            if (result>valor_mayor):
-                valor_mayor=result
-                lista_mayor=lista
-            else:
-                lista=[] 
-    return (valor_mayor,lista_mayor)             
+#def buscarVertical(info):
+#    i=0
+#    j=0
+#    c=0
+#    multi=1
+#    valor_mayor=-1
+#    lista= []
+#    lista_mayor=[]
+#    for i in range (17):
+#        for j in range (20):
+#            for c in range (4):
+#                lista=lista+ [i+c,j,data[i+c][j]]
+#                multi=multi*data[i+c][j]
+#            result=multi
+#            multi=1
+#            if (result>valor_mayor):
+#                valor_mayor=result
+#                lista_mayor=lista
+#            else:
+#                lista=[] 
+#    return (valor_mayor,lista_mayor)             
             
 # Esta función cumple el mismo principio de busqueda que las dos funciones
 # anteriores variando solo la variable "c" y limitando filas y columnas hasta 
 # los casilleros 17
-def buscarDiagonal():
-    i=0
-    j=0
-    c=0
-    multi=1
-    valor_mayor=-1
-    lista= []
-    lista_mayor=[]
-    for i in range (17):
-        for j in range (17):
-            for c in range (4):
-                lista=lista+ [i+c,j+c,data[i+c][j+c]]
-                multi=multi*data[i+c][j+c]
-            result=multi
-            multi=1
-            if (result>valor_mayor):
-                valor_mayor=result
-                lista_mayor=lista
-            else:
-                lista=[]
-    return (valor_mayor,lista_mayor) 
+            
+#def buscarDiagonal(info):
+#    i=0
+#    j=0
+#    c=0
+#    multi=1
+#    valor_mayor=-1
+#    lista= []
+#    lista_mayor=[]
+#    for i in range (17):
+#        for j in range (17):
+#            for c in range (4):
+#                lista=lista+ [i+c,j+c,data[i+c][j+c]]
+#                multi=multi*data[i+c][j+c]
+#            result=multi
+#            multi=1
+#            if (result>valor_mayor):
+#                valor_mayor=result
+#                lista_mayor=lista
+#            else:
+#                lista=[]
+#    return (valor_mayor,lista_mayor) 
     
 # Recibe los resultados de cada busqueda y devuelve el de mayor resultado
-def comparar (horizontal,vertical,diagonal):
-    if(horizontal[0]>vertical[0] and horizontal[0]>diagonal[0]):
-        return horizontal
-    else:
-        if(vertical[0]>diagonal[0] and vertical[0]>horizontal[0]):
-            return vertical
-        else:
-            if(diagonal[0]>vertical[0] and diagonal[0]>horizontal[0]):
-                return diagonal
+#def comparar (horizontal,vertical,diagonal):
+#    if(horizontal[0]>vertical[0] and horizontal[0]>diagonal[0]):
+#        return horizontal
+#    else:
+#        if(vertical[0]>diagonal[0] and vertical[0]>horizontal[0]):
+#            return vertical
+#        else:
+#            if(diagonal[0]>vertical[0] and diagonal[0]>horizontal[0]):
+#                return diagonal
         
 
     
@@ -145,7 +171,14 @@ data=np.genfromtxt(StringIO(data))
 
 distribuirEnP(size)
 
-print ""
+rango=comm.recv(source=0)
+rango=rango-1
+comm.send(rango,dest=0)
+
+if rank==0:
+    buscarRangos()
+
+
 if rank==0:
     info=comm.recv(source=0)
     print "rank 0 = "+ str(info)
@@ -159,22 +192,23 @@ if rank==3:
     info=comm.recv(source=0)
     print "rank 3 = "+ str(info)
 
-horizontal=buscarHorizontal()
-vertical=buscarVertical()
-diagonal=buscarDiagonal()
+    
+
+
+#horizontal=buscarHorizontal()
+#vertical=buscarVertical(info)
+#diagonal=buscarDiagonal(info)
 
 if rank==0:
-    mejor=comparar(horizontal,vertical,diagonal)
-    
-    print ""
-    print "--Result--"
-    print ""
-    print "Matriz["+str(mejor[1][0])+"]["+str(mejor[1][1])+"]="+str(mejor[1][2])
-    print "Matriz["+str(mejor[1][3])+"]["+str(mejor[1][4])+"]="+str(mejor[1][5])
-    print "Matriz["+str(mejor[1][6])+"]["+str(mejor[1][7])+"]="+str(mejor[1][8])
-    print "Matriz["+str(mejor[1][9])+"]["+str(mejor[1][10])+"]="+str(mejor[1][11])
-    print ""
-    print str(mejor[1][2])+" x "+str(mejor[1][5])+" x "+str(mejor[1][8])+" x "+str(mejor[1][11])+" = "+str(mejor[0])
+#    mejor=comparar(horizontal,vertical,diagonal)
+#    print "--Result--"
+#    print ""
+#    print "Matriz["+str(mejor[1][0])+"]["+str(mejor[1][1])+"]="+str(mejor[1][2])
+#    print "Matriz["+str(mejor[1][3])+"]["+str(mejor[1][4])+"]="+str(mejor[1][5])
+#    print "Matriz["+str(mejor[1][6])+"]["+str(mejor[1][7])+"]="+str(mejor[1][8])
+#    print "Matriz["+str(mejor[1][9])+"]["+str(mejor[1][10])+"]="+str(mejor[1][11])
+#    print ""
+#    print str(mejor[1][2])+" x "+str(mejor[1][5])+" x "+str(mejor[1][8])+" x "+str(mejor[1][11])+" = "+str(mejor[0])
             
     
     #Calculo de tiempo
